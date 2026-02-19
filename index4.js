a461785374bb5cdecf178e6cd9b2e0713c732bd9
iashish97 <ashishmohan27622@gmail.com>
2026-02-19 13:33:39 +0530

    git commit

---
+++ index4.js
@@ -0,0 +1,18 @@
+import express from'express'
+const app=express()
+app.listen(8080,()=>console.log("Server started"))
+
+app.get("/:id/:email",(req,res)=>{
+    console.log(req.url)
+    console.log(req.params)
+    res.send(req.params.id+" "+req.params.email)
+})
+app.get("/id/:id/email/:email",(req,res)=>{
+    console.log(req.url)
+    console.log(req.params)
+    res.send(req.params.id+" "+req.params.email)
+})
+
+app.get("/home",(req,res)=>{
+    res.send("Hello World")
+})
